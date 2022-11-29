56e55839cdea78e7c7dc2a6713151bff39e2e6de
ravina-ghidode <ravina.ghidode@newvision-software.com>
2022-11-29 11:01:09 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ResturantDashComponent } from './resturant-dash/resturant-dash.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo : 'login',
+    pathMatch:'full'
+
+  },
+  {
+    path:'login',
+    component:LoginComponent
+
+},
+{
+  path:'signup',
+  component:SignupComponent
+
+},
+{
+  path:'restaurant',
+  component:ResturantDashComponent
+
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
